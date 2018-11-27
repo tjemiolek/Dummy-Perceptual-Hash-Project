d5d609d170bfa47e5264018465a6d1ea94d2378d
tjemiolek <45403942+tjemiolek@users.noreply.github.com>
2018-11-27 20:59:38 +0100

    Add files via upload

---
+++ sql/Procedury składowane/dbo.sp_get_similar_photo.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[sp_get_similar_photo] @param1 varchar(8)
+AS
+
+BEGIN
+
+;WITH CTETable (ImgPath, Distance, Dph)
+AS (
+SELECT id.ImagePath, dbo.f_get_distance(@param1, id.Dph) AS result, id.Dph 
+FROM ImageData id
+)
+
+SELECT ImgPath
+FROM CTETable
+WHERE Distance <=4
+ORDER BY Dph Asc;
+END
+
+RETURN
